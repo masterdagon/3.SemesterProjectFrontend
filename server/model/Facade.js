2ed546809251a5464a2dd2dbcb10d910f0b32b0a
Muggi <masterdagon@hotmail.com>
2015-05-12 14:07:52 +0200

    added create user to facade

--- server/model/Facade.js
+++ server/model/Facade.js
@@ -1,3 +1,19 @@
 /**
  * Created by Dennnis on 12-05-2015.
  */
+
+var db = require("./db");
+var mongoose = require('mongoose');
+var user = mongoose.model('Customer');
+var airline = mongoose.model('Airline');
+
+function createUser(userName, email, pw) {
+    var newuser = {
+        userName: userName,
+        email: email,
+        pw: pw,
+        tickets: []
+    }
+    user.save(newuser);
+}
+
