9147869dce8affb842e6a8f8366cf2c3571e6514
Dennis <Dennisheegaard@gmail.com>
2015-05-14 16:28:51 +0200

    facade populate

--- server/model/Facade.js
+++ server/model/Facade.js
@@ -6,7 +6,7 @@ var db = require("./db");
 var bcrypt = require('bcryptjs');
 var mongoose = require('mongoose');
 var user = mongoose.model('User');
-var airline = mongoose.model('Airline');
+var server = mongoose.model('Server');
 var request = require('request');
 
 function createUser(userName, email, pw, callback) {
@@ -94,83 +94,89 @@ function findUser(userName, callback) {
     })
 }
 
-function getAirlineUrls(callback) {
+function getServerUrls(callback) {
     var urls = [];
-    airline.find({}, function (err, airlines) {
+    server.find({}, function (err, server) {
         if (err) {
             callback(err);
         } else {
-            //for (var i in airlines) {
-            //    urls.push(airlines[i].url);
-            //}
-            callback(err, airlines);
+            callback(err, server);
         }
     });
 
 }
 
-function createAirline(name, url, callback) {
+function createServer(name, url, callback) {
     console.log('name= ' + name)
     console.log('url= ' + url)
-    var newAirline = new airline({
+    var newServer = new airline({
         name: name,
         url: url
     });
-    newAirline.save(function (err) {
+    newServer.save(function (err) {
         if (err) {
             callback(err);
         } else {
-            callback(err, newAirline)
+            callback(err, newServer)
         }
     })
 }
 function post_reservation_flightID(name, flightId, customer, callback) {
-    airline.findOne({name: name}, function (err, airline) {
-        if (err) {
-            callback(err)
-        } else {
-            var path = airline.url + flightId;
-            console.log(path)
-            request({
-                    url: path,
-                    method: 'POST',
-                    json: customer
-                }, function (err, res, body) {
-                    console.log(body)
-                    if (!err && res.statusCode == 200) {
-                        callback(null, body);
-                    } else {
-                        callback(err);
+    server.findOne({name: name}, function (err, server) {
+        if (!err) {
+            if(!server){
+                callback({code: 404, message: 'cant find server', description:'the server was not available' })
+            }else{
+                var path = server.url + flightId;
+                console.log(path)
+                request({
+                        url: path,
+                        method: 'POST',
+                        json: customer
+                    }, function (err, res, body) {
+                        console.log(body)
+                        if (!err && res.statusCode == 200) {
+                            callback(null, body);
+                        } else {
+                            callback(err);
+                        }
                     }
-                }
-            );
+                );
+            }
+        } else {
+            callback(err)
+
         }
     })
 }
 
 function get_Departure_Date(departure, date, callback) {
-    getAirlineUrls(function (err, airlines) {
+    getAirlineUrls(function (err, servers) {
         if (err) {
             callback(err)
         } else {
             var storage = [];
             var count = 0;
-            airlines.forEach(function (airline) {
-                var path = airline.url + departure + "/" + date;
+            servers.forEach(function (server) {
+                var path = server.url + departure + "/" + date;
                 request(path, function (err, res, body) {
-                    if (!err && res.statusCode == 200) {
-                        var flight = {name: airline.name, flights: JSON.parse(body)};
-                        if(flight.flights.length > 0){
-                            storage.push(flight);
-                        }
+                    if(err){
+                        callback(err)
+                    }else {
+                        if (res.statusCode == 200) {
+                            var flight = {name: server.name, flights: JSON.parse(body)};
+                            if (flight.flights.length > 0) {
+                                storage.push(flight);
+                            }
 
-                    }
-                    count++;
-                    if (count == airlines.length) {
-                        if(storage.length>0){
-                            callback(null, storage);
-                        }else{
-                            callback(JSON.parse(body));
+                        }
+                        count++;
+                        if (count == servers.length) {
+                            if (storage.length > 0) {
+                                callback(null, storage);
+                            } else {
+                                callback(JSON.parse(body));
+                            }
                         }
                     }
                 })
@@ -180,27 +186,31 @@ function get_Departure_Date(departure, date, callback) {
 }
 
 function get_Departure_Arrival_Date(departure, arrival, date, callback) {
-    getAirlineUrls(function (err, airlines) {
+    getAirlineUrls(function (err, servers) {
         if (err) {
             callback(err)
         } else {
             var storage = [];
             var count = 0;
-            airlines.forEach(function (airline) {
-                var path = airline.url + departure + "/" + arrival + "/" + date;
+            servers.forEach(function (server) {
+                var path = server.url + departure + "/" + arrival + "/" + date;
                 request(path, function (err, res, body) {
-                    if (!err && res.statusCode == 200) {
-                        var flight = {name: airline.name, flights: JSON.parse(body)};
-                        if(flight.flights.length > 0){
-                            storage.push(flight);
+                    if(err){
+                        callback(err)
+                    }else{
+                        if (res.statusCode == 200) {
+                            var flight = {name: server.name, flights: JSON.parse(body)};
+                            if(flight.flights.length > 0){
+                                storage.push(flight);
+                            }
                         }
-                    }
-                    count++;
-                    if (count == airlines.length) {
-                        if(storage.length>0){
-                            callback(null, storage);
-                        }else{
-                            callback(JSON.parse(body));
+                        count++;
+                        if (count == servers.length) {
+                            if(storage.length>0){
+                                callback(null, storage);
+                            }else{
+                                callback(JSON.parse(body));
+                            }
                         }
                     }
                 })
@@ -211,29 +221,33 @@ function get_Departure_Arrival_Date(departure, arrival, date, callback) {
 }
 
 function get_Reservation(name, reservationId, callback) {
-    airline.findOne({name: name}, function (err, airline) {
-        if (err) {
+    server.findOne({name: name}, function (err, server) {
+        if (!err) {
+            if(!server){
+                callback({code: 404, message: 'Invalid ReservationID', description:'the reservation could not be found' })
+            }else{
+                var path = server.url + reservationId;
+                request(path, function (err, res, body) {
+                    if (!err && res.statusCode == 200) {
+                        callback(null, JSON.parse(body));
+                    } else {
+                        callback(JSON.parse(body));
+                    }
+                })
+            }
+        }else{
             callback(err)
-        } else {
-            var path = airline.url + reservationId;
-            request(path, function (err, res, body) {
-                if (!err && res.statusCode == 200) {
-                    callback(null, JSON.parse(body));
-                } else {
-                    callback(JSON.parse(body));
-                }
-            })
         }
     });
 }
 
 function delete_Reservation(name, reservationId, callback) {
-    airline.findOne({name: name}, function (err, airline) {
+    server.findOne({name: name}, function (err, server) {
         if(!err){
-            if (!airline) {
-                callback({code: 3, message: 'Invalid ReservationID', description:'the reservation could not be found' })
+            if (!server) {
+                callback({code: 404, message: 'Invalid ReservationID', description:'the reservation could not be found' })
             } else {
-                var path = airline.url + reservationId;
+                var path = server.url + reservationId;
                 request.del(path, function (err, res, body) {
                     if(err){
                         callback(err)
