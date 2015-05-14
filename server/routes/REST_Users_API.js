c9faea008ea6a4f88324fe67ce9ef3ebf90c7424
Muggi <masterdagon@hotmail.com>
2015-05-14 13:34:19 +0200

    user api get departure date

--- server/routes/REST_Users_API.js
+++ server/routes/REST_Users_API.js
@@ -7,4 +7,17 @@ router.get('/test', function(req, res) {
     res.end('{"msg" : "Test Message fetched from the server, You are logged on as a User since you could fetch this data"}');
 });
 
+router.get('/:departure/:date'),function(req,res){
+    var departure = req.params.departure;
+    var date = req.params.date;
+    facade.get_Departure_Date(departure,date,function(err,array){
+        if(err){
+
+        }else{
+            res.header("Content-type","application/json");
+            res.end(JSON.stringify(array));
+        }
+    })
+}
+
 module.exports = router;
