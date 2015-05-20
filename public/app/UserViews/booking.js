b11b058576e8cbbc47c02a014f772d66c4f3f291
Muggi <masterdagon@hotmail.com>
2015-05-20 22:56:46 +0200

    booking

--- public/app/UserViews/booking.js
+++ public/app/UserViews/booking.js
@@ -25,11 +25,11 @@ angular.module('airportApp.booking', ['ngRoute'])
             var payload = {
                 Passengers : customers
             };
-            for(var x = 0; x < payload.passengers.length;x++){
-                console.log(payload.passengers[x].firstName);
+            for(var x = 0; x < payload.Passengers.length;x++){
+                console.log(payload.Passengers[x].firstName);
             }
             console.log("SERVERNAME: " + serverName + " - FLIGHTID: " + flightId);
-            userFactory.postReservation(serverName,flightId,customers)
+            userFactory.postReservation(serverName,flightId,payload)
                 .success(function (data, status, headers, config) {
                     $scope.reservation = data;
                     $scope.error = null;
