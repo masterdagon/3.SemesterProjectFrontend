4c41b3b3cfbfde7143e30c46e5aaf5664f0c069b
Muggi <masterdagon@hotmail.com>
2015-05-26 00:29:00 +0200

    minor fixes

--- public/app/UserViews/DepartureDate.js
+++ public/app/UserViews/DepartureDate.js
@@ -34,9 +34,11 @@ angular.module('airportApp.departureDate', ['ngRoute'])
                 $scope.checkArrival = false;
             }
             if (!$scope.more && !$scope.checkDate && !$scope.checkDeparture) {
+                $scope.searching = true;
                 departureDate($scope.departure,$scope.dDate);
             }
             if($scope.more && !$scope.checkDate && !$scope.checkDeparture && !$scope.checkArrival){
+                $scope.searching = true;
                 departureArrivalDate($scope.departure,$scope.arrival,$scope.dDate);
             }
         };
@@ -48,6 +50,7 @@ angular.module('airportApp.departureDate', ['ngRoute'])
             var ms = date.getTime();
             userFactory.getDepartureDate(departure, ms)
                 .success(function (data, status, headers, config) {
+                    $scope.searching = false;
                     $scope.info = data;
                     $scope.error = null;
                 }).
@@ -68,6 +71,7 @@ angular.module('airportApp.departureDate', ['ngRoute'])
             var ms = date.getTime();
             userFactory.getDepartureDateArrival(departure,arrival,ms)
                 .success(function (data, status, headers, config) {
+                    $scope.searching = false;
                     $scope.info = data;
                     $scope.error = null;
                 }).
